50c069b6405c688177fe068d90c0ee6991cde554
Alfredo Poveda <alfpovsistemas@gmail.com>
2021-04-19 23:45:08 -0400

    Added support to 'cors', 'compression', 'helmet'

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,21 @@
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import * as compression from 'compression';
+import * as helmet from 'helmet';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
 
   app.enableCors();
-
-  const configService = app.get(ConfigService);
+  app.use(compression());
+  app.use(
+    helmet({
+      referrerPolicy: configService.get('app.referrerPolicy'),
+      contentSecurityPolicy: configService.get('app.contentSecurityPolicy'),
+    }),
+  );
 
   await app.listen(configService.get('app.port'));
 }
