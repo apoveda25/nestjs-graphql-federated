b8859d9eb507c607ea1a094c6a46e9164be4597d
Alfredo Poveda <alfpovsistemas@gmail.com>
2021-04-22 07:40:51 -0400

    build(Added support to Apollo Studio)

--- src/config/graphql-federated.config.ts
+++ src/config/graphql-federated.config.ts
@@ -1,5 +1,6 @@
 import { ConfigService } from '@nestjs/config';
 import { GqlModuleOptions } from '@nestjs/graphql';
+import { join } from 'path';
 
 export const graphqlFederatedConfig: (
   configService: ConfigService,
@@ -7,7 +8,10 @@ export const graphqlFederatedConfig: (
   introspection: configService.get('graphql.introspection'),
   playground: configService.get('graphql.playground'),
   subscriptions: configService.get('graphql.subscriptions'),
-  autoSchemaFile: configService.get('graphql.autoSchemaFile'),
+  autoSchemaFile: join(
+    process.cwd(),
+    configService.get('graphql.autoSchemaFile'),
+  ),
   sortSchema: configService.get('graphql.sortSchema'),
   context: ({ req }) => {
     return {
